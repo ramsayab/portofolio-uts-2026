d05b9fe3853524ce1412c06e242d96212e1103f2
root <root@DESKTOP-GSV83RI.localdomain>
2026-05-18 16:41:29 +0700

    done db project

--- src/database/seeders/PortofolioSeeder.php
+++ src/database/seeders/PortofolioSeeder.php
@@ -16,19 +16,35 @@ class PortofolioSeeder extends Seeder
 
         Portofolio::query()->insert([
             [
-                'nama_project' => 'Analisis Sentimen Produk',
-                'deskripsi_project' => 'Proyek analisis sentimen untuk mengelompokkan ulasan pengguna berdasarkan sentimen positif, netral, dan negatif.',
-                'link_github' => 'https://github.com/username/analisis-sentimen-produk',
+                'nama_project' => 'clash of clans auto farm',
+                'deskripsi_project' => 'menggabungkan ml/ai stack untuk membuat program rumit:
+                yolo, cv2, pydirectinput, mss. serta customisasi dataset untuk hasil model lebih akurat',
+                'link_github' => 'https://github.com/ramsayab/COC_AUTOFARM',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
-                'nama_project' => 'Prediksi Harga Rumah',
-                'deskripsi_project' => 'Model machine learning untuk memprediksi harga rumah berdasarkan fitur lokasi, luas bangunan, dan fasilitas.',
-                'link_github' => 'https://github.com/username/prediksi-harga-rumah',
+                'nama_project' => 'chess war',
+                'deskripsi_project' => 'Proyek yang menggabungkan chess tradisional dengan tambahan power dan unique move supaya lebih banyak variable.',
+                'link_github' => 'https://github.com/ramsayab/chess_war-2026',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
+            [
+                'nama_project' => 'flowchart generator',
+                'deskripsi_project' => 'menggunakan library turtle menggambar flowchart dengan lebih dari 11 diagram',
+                'link_github' => 'https://github.com/ramsayab/flowchart_generator',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'nama_project' => 'typing cheat',
+                'deskripsi_project' => 'menggunakan selenium untuk membuka chrome dan langsung mengambil rangkaian kata lewat div dengan teknik scraping',
+                'link_github' => 'https://github.com/ramsayab/Online_Typing_Test_Cheat',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
         ]);
     }
 }
